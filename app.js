2df6535e78d781962e5218aa349570c221eaf0d9
iRock <47889456+rocaN5@users.noreply.github.com>
2024-10-07 13:20:46 +0300

    v.1.12.d21

--- app.js
+++ app.js
@@ -311,7 +311,7 @@ function convertToImageAndOpenInNewTab() {
 
 // Функция для отправки изображения в Telegram
 function sendImageToTelegram() {
-  const token = '7095204830:AAFx3-UnTed0mJbQ0Fh7NgomKUwDEEo8JtE';
+  const token = '7982110225:AAFDbut0kNIPmPbyLghuXFfhkYQ3u7N-PXk';
   const chatId = '-1002405934260';
   const imgElement = document.querySelector('img.test-img');
   const captionInputText = document.getElementById('qr-text')?.value || ''; // Получаем значение из инпута
